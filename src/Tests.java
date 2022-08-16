299dcc61742fd055e0f3128811543a82aeedad15
P1k4sh <hsakip@gmail.com>
2022-08-17 00:12:01 +0300

    Initial commit

---
+++ src/Tests.java
@@ -0,0 +1,72 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Tests {
+    public static void main(String[] args) {
+        int[][] array = fillArray();
+        printArray((array));
+        printArray(deleteRowColumnWithMinElement(array, findMinElement(array)));
+    }
+
+    static int[][] fillArray() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int m = sc.nextInt();
+        System.out.print("Enter number of columns: ");
+        int n = sc.nextInt();
+        int[][] array = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                array[i][j] = (int) (Math.random() * 100);
+            }
+        }
+        return array;
+    }
+
+    static void printArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    static int[] findMinElement(int[][] array) {
+        int min = array[0][0];
+        int indexIOfMin = 0;
+        int indexJOfMin = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (array[i][j] < min) {
+                    min = array[i][j];
+                    indexIOfMin = i;
+                    indexJOfMin = j;
+                }
+            }
+        }
+        int[] indexes = {indexIOfMin, indexJOfMin};
+        System.out.println("Min element is " + min + ", i = " + indexIOfMin + ", j = " + indexJOfMin);
+        System.out.println(Arrays.toString(indexes));
+        return indexes;
+    }
+
+    static int[][] deleteRowColumnWithMinElement(int[][] array, int[] indexes) {
+
+        int[][] editedArr = new int[array.length][array[0].length];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                if (i == indexes[0] || j == indexes[1]) {
+                    continue;
+                } else {
+                    editedArr[i][j] = array[i][j];
+                }
+            }
+        }
+        return editedArr;
+
+
+    }
+}
